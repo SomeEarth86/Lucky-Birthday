44a47eff637ee1076813fc06420c7e48e497113c
Samarth Krishna Joshi <skrishnaj86@gmail.com>
2021-08-18 11:36:19 +0530

    feat: tweaking and final touch

---
+++ app.js
@@ -0,0 +1,43 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-num");
+const checkNumberBtn = document.querySelector("#check-btn");
+const messageDisplay = document.querySelector("#message");
+
+checkNumberBtn.addEventListener("click", function checkBdayLucky(){
+
+    if(dateOfBirth.value && luckyNumber.value){
+        const dobNum = dobToNumberConversion(dateOfBirth.value);//it will get number as result from dob string
+        const dobSum = getDobSum(dobNum);
+        // console.log(dobNum,dobSum);
+        checkLuckyFinal(dobSum,luckyNumber.value);
+    }
+
+});
+
+function checkLuckyFinal (dobSum,luckynum){
+
+    if(dobSum % luckynum === 0){
+        showMessage(" YAAAS, You're Lucky 🥳🤩😎, Sheeeeessh 🥴👈")
+        console.log("Hellluuuu");
+    }
+    else{
+        showMessage(" Naah Dawg, You ain't lucky Bruh!!! 💀☠️")
+    }
+}
+
+function showMessage (text){
+    messageDisplay.innerText = text;
+}
+
+function dobToNumberConversion(dob){
+    return dob.replaceAll('-',"");  
+}
+
+function getDobSum (dobNum){
+    let sum = 0;
+    for(i=0;i<dobNum.length;++i)
+    {
+        sum = sum + Number(dobNum.charAt(i));
+    }
+    return sum;
+}
